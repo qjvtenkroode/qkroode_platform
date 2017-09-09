e2c18f980ba9c6da28bba6808f42254bc591e3b2
Quincey ten Kroode <qjv.tenkroode@gmail.com>
2017-09-09 22:09:29 +0200

    changes to exception handling

--- qkroode_platform/services/baseService.py
+++ qkroode_platform/services/baseService.py
@@ -7,21 +7,21 @@ class BaseService(MethodView):
 
     @requires_auth
     def delete(self):
-        raise ServiceException("Method not implemented.", status_code=501)
+        raise ServiceException('Method not implemented.', status_code=501)
 
     @requires_auth
     def get(self):
-        raise ServiceException("Method not implemented.", status_code=501)
+        raise ServiceException('Method not implemented.', status_code=501)
 
     @requires_auth
     def head(self):
-        raise ServiceException("Method not implemented.", status_code=501)
+        raise ServiceException('Method not implemented.', status_code=501)
 
     @requires_auth
     def post(self):
-        raise ServiceException("Method not implemented.", status_code=501)
+        raise ServiceException('Method not implemented.', status_code=501)
 
     @requires_auth
     def put(self):
-        raise ServiceException("Method not implemented.", status_code=501)
+        raise ServiceException('Method not implemented.', status_code=501)
 
